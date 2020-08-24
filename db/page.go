41251649cae5e49441fd11420c5f53c48b5708e7
Ron <271154390@qq.com>
2020-08-24 20:36:10 +0800

    添加k/v数据库，内存映射文件。

---
+++ db/page.go
@@ -0,0 +1,10 @@
+//Package db one page represent a read write unit.
+//Copyright (C) 2020 to All Authors. all rights reserved.
+//Author Ron
+//Date 2020/08/24 16:17:17
+//Version 1.0
+package db
+
+type page struct {
+	id uint64
+}
